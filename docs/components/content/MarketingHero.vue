68dd9458776bde04453b02fb488742d49aabdd4f
Dan Pastori <dan@521dimensions.com>
2023-05-02 10:54:53 -0700

    Initial commit of the docs site

---
+++ docs/components/content/MarketingHero.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="mx-auto max-w-[940px] flex flex-col px-6 py-32 lg:py-48">
+        <h1 class="font-inter text-[32px] leading-[32px] text-white font-bold lg:text-center lg:text-[64px] lg:leading-[64px]">
+            You won't believe managing servers was <span class="text-[#1CE783]">this easy</span>.
+        </h1>
+
+        <p class="text-slate-300 font-inter text-xl mt-4 lg:text-2xl lg:mt-8 lg:text-center">Exit big cloud and break free from vendor lock. Configure your infrastructure in minutes and replicate your environment from development to production.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
