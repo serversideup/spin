f61011836970eda69dd8817e7422df564c8644ab
Jay Rogers <jay@521dimensions.com>
2023-09-27 12:20:43 -0500

    Improved update logic

--- lib/actions/update.sh
+++ lib/actions/update.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 action_update() {
-  if is_installed_to_user; then
-
-    if is_internet_connected; then
-      check_for_upgrade --force
+    if is_installed_to_user && not_in_active_development && is_internet_connected; then
+        check_for_upgrade --force
+    else
+      printf "${BOLD}${YELLOW}⚠️ Cannot automatically perform an update.${RESET} "
+      printf "You're using \"spin\" from the project level or you're in active development. Install updates via NPM or Composer instead.\n"
+      exit 1
     fi
-
-  else
-    printf "${BOLD}${YELLOW}⚠️ Cannot automatically perform an update.${RESET} "
-    printf "Your using \"spin\" from the project level. Install updates via NPM or Composer instead.\n"
-    exit 1
-  fi
 }
