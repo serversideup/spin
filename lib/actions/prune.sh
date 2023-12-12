8dc63d782cb20a58d6562f8b3a30c4c2fdebcf17
Jay Rogers <jay@521dimensions.com>
2023-12-12 08:27:38 -0600

    Clear spin cache

--- lib/actions/prune.sh
+++ lib/actions/prune.sh
@@ -3,4 +3,6 @@ action_prune(){
   printf "${BOLD}${YELLOW}🚨 You're about to delete some data.${RESET}"
   echo
   docker system prune --all $@
+  # Clear spin cache
+  rm -rf $SPIN_CACHE_DIR/.spin*
 }
