7d32f80fa581f753b3f7dde60abb0ca4c10811e4
Dan Pastori <dan@521dimensions.com>
2023-12-14 13:46:40 -0800

    Nuxt Upgrade, OG Images, ready for testing

--- docs/components/content/CodePanel.vue
+++ docs/components/content/CodePanel.vue
@@ -8,7 +8,7 @@
 
         <div class="group bg-white/2.5">
             <div class="relative overflow-x-auto p-4 text-xs text-white">
-                <ContentSlot :use="$slots.default" unwrap="pre"/>
+                <ContentSlot :use="$slots.default"/>
                 <CopyButton :code="$slots.default()[0].props.code"/>
             </div>
         </div>
