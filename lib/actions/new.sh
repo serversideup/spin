f86f9d32a7f0a93b18b47c519121f28dccd3062c
Jay Rogers <jay@521dimensions.com>
2023-12-14 10:18:22 -0600

    Refactor project creation commands

--- lib/actions/new.sh
+++ lib/actions/new.sh
@@ -9,14 +9,14 @@ action_new(){
           latest_image=$(get_latest_image php)
           docker pull $latest_image
           docker run --rm -v $(pwd):/var/www/html -e "LOG_LEVEL=off" $latest_image composer create-project laravel/laravel "$@"
-          add_spin_to_project php ${@:-laravel}
+          install_spin_package_to_project php ${@:-laravel}
         ;;
         nuxt)
           shift 1
           latest_image=$(get_latest_image node)
           docker pull $latest_image
           docker run --rm -it -v $(pwd):/usr/src/app -w /usr/src/app $latest_image npx nuxi@latest init "$@"
-          add_spin_to_project node ${@:-"nuxt-app"}
+          install_spin_package_to_project node ${@:-"nuxt-app"}
         ;;
         *)
           echo "\"$1\" is not a valid command. Below are the commands available."
