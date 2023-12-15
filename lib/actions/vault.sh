c15c31113c670cb8ab8356fac8eac5b1719a363a
Jay Rogers <jay@521dimensions.com>
2023-12-15 11:37:05 -0600

    Added spin vault

---
+++ lib/actions/vault.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+action_vault(){
+    if [[ $1 == "edit" ]]; then
+
+        if [[ -z $2 ]];then
+            echo "${BOLD}${RED}❌ Please specify a file to edit.${RESET}"
+            return 1
+        fi
+
+        echo "${BOLD}${YELLOW}ℹ️ We're using ansible-vault within the container to edit your file, which uses \"vi\".${RESET}"
+        echo "${BOLD}${YELLOW}👉 To edit the file, press i.${RESET}"
+        echo "${BOLD}${YELLOW}💾 To save your changes and exit, press ESC, then type \":wq\" and press ENTER.${RESET}"
+    fi
+    docker run --name spin-ansible --rm -it -v "$(pwd)":/ansible $DEFAULT_ANSIBLE_IMAGE ansible-vault "$@"
+}
