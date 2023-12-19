dda59338dfe4865cabc2b876a69aced529dcbdf8
Jay Rogers <jay@521dimensions.com>
2023-12-19 10:14:48 -0600

    Refactor vault.sh script to handle local and Docker environments

--- lib/actions/vault.sh
+++ lib/actions/vault.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 action_vault(){
-    if [[ $1 == "edit" ]]; then
+    # Sanity check
+    if [[ $1 == "edit" && -z $2 ]]; then
+        echo "${BOLD}${RED}❌ Please specify a file to edit.${RESET}"
+        return 1
+    fi
 
-        if [[ -z $2 ]];then
-            echo "${BOLD}${RED}❌ Please specify a file to edit.${RESET}"
-            return 1
-        fi
-
-        echo "${BOLD}${YELLOW}ℹ️ We're using ansible-vault within the container to edit your file, which uses \"vi\".${RESET}"
+    # Check if ansible-vault is installed locally
+    if [[ $(command -v ansible-vault)  ]]; then
+        vault_run_command="ansible-vault"
+        run_type="local"
+    else
+        vault_run_command="docker run --rm -it -v $(pwd):/ansible $SPIN_ANSIBLE_IMAGE ansible-vault"
+        run_type="docker"
+    fi
+    
+    # Show notification for users running with Docker
+    if [[ $run_type == "docker" && $1 == "edit" ]]; then
+        echo "${BOLD}${YELLOW}ℹ️ You don't have ansible-vault installed locally."
+        echo "${BOLD}${YELLOW}ℹ️ We'll use the container to edit your file, which uses \"vi\".${RESET}"
         echo "${BOLD}${YELLOW}👉 To edit the file, press i.${RESET}"
         echo "${BOLD}${YELLOW}💾 To save your changes and exit, press ESC, then type \":wq\" and press ENTER.${RESET}"
     fi
-    docker run --rm -it -v "$(pwd)":/ansible $SPIN_ANSIBLE_IMAGE ansible-vault "$@"
+
+    $vault_run_command "$@"
 }
