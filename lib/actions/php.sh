a7c7a312f26eb2d3a35e8819bcb1425382142443
Jay Rogers <jay@521dimensions.com>
2023-12-21 09:19:57 -0600

    Add PHP action and default users

---
+++ lib/actions/php.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+action_php(){
+  docker_pull_check "$@"
+
+  local args=($(filter_out_spin_arguments "$@"))
+
+  # Run Docker Compose without dependencies. Ensure automations and S6 logging are disabled
+  $COMPOSE_CMD run --remove-orphans --no-deps --rm \
+    --entrypoint '' \
+    -e "LOG_LEVEL=off" \
+    --user $SPIN_DEFAULT_PHP_USER \
+    $SPIN_DEFAULT_PHP_SERVICE_NAME \
+    "${args[@]}"
+}
