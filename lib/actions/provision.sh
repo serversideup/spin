b8e6c3f9803f61113e5e1a7e661908bbb51decd8
Jay Rogers <jay@521dimensions.com>
2023-12-27 11:17:43 -0600

    Add support for remote user argument in provision script

--- lib/actions/provision.sh
+++ lib/actions/provision.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 action_provision(){
+    additional_ansible_args=""
+    remote_user_arg=""
+
+    # Check if vault password exists
     if [[ -f .vault-password ]]; then
         additional_ansible_args="--vault-password-file .vault-password"
     elif is_encrypted_with_ansible_vault ".spin.yml" && is_encrypted_with_ansible_vault ".spin-inventory.ini"; then
         additional_ansible_args="--ask-vault-password"
-    else
-        additional_ansible_args=""
     fi
+
+    # Process arguments
+    while [[ "$#" -gt 0 ]]; do
+        case "$1" in
+            --user|-u)
+                remote_user_arg="--ask-become-pass --extra-vars ansible_user=$2"
+                shift 2
+                ;;
+        esac
+    done
     
     # Install the required Ansible roles
     run_ansible ansible-galaxy collection install serversideup.spin --upgrade
@@ -15,6 +27,7 @@ action_provision(){
     run_ansible ansible-playbook serversideup.spin.provision \
         --inventory ./.spin-inventory.ini \
         --extra-vars @./.spin.yml \
+        $remote_user_arg \
         $additional_ansible_args \
         "$@"
 }
