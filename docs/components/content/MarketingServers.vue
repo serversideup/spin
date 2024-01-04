e240eed2fa4f0e566c3c8d4e1471daf3156dae12
Jay Rogers <jay@521dimensions.com>
2024-01-04 09:24:53 -0600

    Changed video location

--- docs/components/content/MarketingServers.vue
+++ docs/components/content/MarketingServers.vue
@@ -6,7 +6,7 @@
         <div class="px-4">
             <div class="rounded-[10px] mx-auto max-w-[940px] my-8 md:my-16 overflow-hidden">
                 <video autoplay muted loop>
-                    <source src="https://serversideup.net/wp-content/uploads/2024/01/spin-demo_spin-provision.mp4"/>
+                    <source src="https://spin-public-assets.serversideup.net/spin-demo_spin-provision.mp4"/>
                 </video>
             </div>
         </div>
--- docs/components/content/MarketingVideo.vue
+++ docs/components/content/MarketingVideo.vue
@@ -2,7 +2,7 @@
     <div class="px-4">
         <div class="rounded-[10px] mx-auto max-w-[940px] my-8 md:my-16 overflow-hidden">
             <video autoplay muted loop>
-                <source src="https://serversideup.net/wp-content/uploads/2024/01/spin-demo_spin-up.mp4"/>
+                <source src="https://spin-public-assets.serversideup.net/spin-demo_spin-up.mp4"/>
             </video>
         </div>
     </div>
