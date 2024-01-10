6ac9e3ddbb49452a5743e83d5a1e6cbd52ed8e61
Jay Rogers <jay@521dimensions.com>
2024-01-09 22:08:29 -0600

    Fixed GitHub link

--- docs/components/Global/ServerSideUp.vue
+++ docs/components/Global/ServerSideUp.vue
@@ -165,7 +165,7 @@
                     </NuxtLink>
                     
                     <NuxtLink 
-                        :to="'https://github.com/serversideup/docker-php'"
+                        :to="'https://github.com/serversideup/spin'"
                         class="text-slate-300 hover:text-white"
                         target="_blank">
                         <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
