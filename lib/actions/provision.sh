a2007cefb9fbff1fd58c6fbc76b2be47187f0bac
Jay Rogers <jay@521dimensions.com>
2024-05-07 10:39:11 -0500

    Centralized funciton

--- lib/actions/provision.sh
+++ lib/actions/provision.sh
@@ -3,28 +3,8 @@ action_provision(){
     additional_ansible_args=""
     remote_user_arg=""
 
-    # Check if vault password exists
-    if [[ -f .vault-password ]]; then
-        additional_ansible_args="--vault-password-file .vault-password"
-    elif is_encrypted_with_ansible_vault ".spin.yml" && is_encrypted_with_ansible_vault ".spin-inventory.ini"; then
-        additional_ansible_args="--ask-vault-password"
-    fi
+    prepare_ansible_run
 
-    # Process arguments
-    while [[ "$#" -gt 0 ]]; do
-        case "$1" in
-            --user|-u)
-                remote_user_arg="--ask-become-pass --extra-vars ansible_user=$2"
-                shift 2
-                ;;
-        esac
-    done
-    
-    # Install the required Ansible roles
-    run_ansible --allow-ssh --mount-path $(pwd) \
-        ansible-galaxy collection install serversideup.spin --upgrade
-
-    # Run the playbook
     run_ansible --allow-ssh --mount-path $(pwd) \
         ansible-playbook serversideup.spin.provision \
         --inventory ./.spin-inventory.ini \
