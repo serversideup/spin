bfe0bb095ba32a2bb14581413aaa34198ec7bb35
Jay Rogers <jay@521dimensions.com>
2024-05-11 06:35:46 -0500

    Added vault password support

--- lib/actions/vault.sh
+++ lib/actions/vault.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 action_vault(){
-
+    local vault_args=()
+    local vault_run_command=()
     show_help() {
-        $vault_run_command --help | sed 's/ansible-vault/spin vault/g'
+        "${vault_run_command[@]}" --help | sed 's/ansible-vault/spin vault/g'
     }
+
+    # Read the vault arguments into an array
+    read -r -a vault_args < <(ansible_vault_args)
 
     # Check if ansible-vault is installed locally
     if [[ $(command -v ansible-vault)  ]]; then
-        vault_run_command="ansible-vault"
+        vault_run_command=("ansible-vault")
         run_type="local"
     else
-        vault_run_command="run_ansible --mount-path $(pwd) ansible-vault"
+        vault_run_command=("run_ansible" "--mount-path" "$(pwd)" "ansible-vault")
         run_type="docker"
     fi
 
     # Check if any argument is '--help'
     for arg in "$@"; do
-        if [ "$arg" = "--help" ]; then
+        if [[ "$arg" == "--help" ]]; then
             show_help
             exit 0
         fi
@@ -37,5 +41,5 @@ action_vault(){
         echo "${BOLD}${YELLOW}💾 To save your changes and exit, press ESC, then type \":wq\" and press ENTER.${RESET}"
     fi
 
-    $vault_run_command "$@"
+    "${vault_run_command[@]}" "$@" "${vault_args[@]}"
 }
