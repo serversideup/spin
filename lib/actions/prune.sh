d111ca4ce4e0797794ef6ab6c1e66dc2a378f31e
Jay Rogers <jay@521dimensions.com>
2024-10-10 16:38:55 -0500

    Set prune to clear collections and registry cache

--- lib/actions/prune.sh
+++ lib/actions/prune.sh
@@ -3,6 +3,24 @@ action_prune(){
   echo "${BOLD}${YELLOW}🚨 You're about to delete some data.${RESET}"
   docker system prune --all $@
   echo "${BOLD}${GREEN}✅ Docker cache cleared.${RESET}"
-  rm -rf $SPIN_CACHE_DIR/.spin*
-  echo "${BOLD}${GREEN}✅ Spin cache cleared.${RESET}"
+
+  if [ -z "$SPIN_CACHE_DIR" ]; then
+    echo "Error: SPIN_CACHE_DIR is not set"
+    exit 1
+  fi
+
+  if compgen -G "$SPIN_CACHE_DIR/.spin*" > /dev/null; then
+    rm -rf "$SPIN_CACHE_DIR"/.spin*
+    echo "${BOLD}${GREEN}✅ Spin update cache cleared.${RESET}"
+  fi
+
+  if [ -d "$SPIN_CACHE_DIR/collections/" ]; then
+    rm -rf $SPIN_CACHE_DIR/collections/
+    echo "${BOLD}${GREEN}✅ Spin Ansible Collections cache cleared.${RESET}"
+  fi
+
+  if [ -d "$SPIN_CACHE_DIR/registry/" ]; then
+    rm -rf $SPIN_CACHE_DIR/registry/
+    echo "${BOLD}${GREEN}✅ Spin Docker Registry cache cleared.${RESET}"
+  fi  
 }
