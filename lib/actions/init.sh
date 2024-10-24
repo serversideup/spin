9d5b9255ef6fe69d13af9a1e2a0bf7f5af3e9ea4
Jay Rogers <jay@521dimensions.com>
2024-10-24 09:08:54 -0500

    Remove encryption

--- lib/actions/init.sh
+++ lib/actions/init.sh
@@ -54,9 +54,7 @@ action_init() {
     copy_template_files "$SPIN_TEMPLATE_TEMPORARY_SRC_DIR/template" "$absolute_project_directory"
 
     # Download default config and inventory from GitHub
-    get_file_from_github_release --repo "serversideup/ansible-collection-spin" --release-type "stable" --src ".spin-inventory.example.ini" --dest "$absolute_project_directory/.spin-inventory.ini"
     get_file_from_github_release --repo "serversideup/ansible-collection-spin" --release-type "stable" --src ".spin.example.yml" --dest "$absolute_project_directory/.spin.yml"
-    prompt_to_encrypt_files --path "$absolute_project_directory" --file ".spin.yml" --file ".spin-inventory.ini"
 
     # Check if the template has a post-install script and execute it
     if [ -f "$SPIN_TEMPLATE_TEMPORARY_SRC_DIR/post-install.sh" ]; then
