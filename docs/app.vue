23991547720b85382cfcb7450f5aa61d8e0bbbda
Jay Rogers <jaydrogers@users.noreply.serversideup.net>
2026-01-26 16:56:24 -0600

    fix: Update favicon paths in app.vue to use relative paths

--- docs/app.vue
+++ docs/app.vue
@@ -43,11 +43,11 @@ useHead({
     { name: 'viewport', content: 'width=device-width, initial-scale=1' }
   ],
   link: [
-    { rel: 'apple-touch-icon', sizes: '180x180', href: '/images/favicon/apple-touch-icon.png' },
-    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/images/favicon/favicon-32x32.png' },
-    { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/images/favicon/favicon-16x16.png' },
-    { rel: 'manifest', href: '/images/favicon/site.webmanifest' },
-    { rel: 'mask-icon', href: '/images/favicon/safari-pinned-tab.svg', color: '#5bbad5' }
+    { rel: 'apple-touch-icon', sizes: '180x180', href: 'images/favicon/apple-touch-icon.png' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'images/favicon/favicon-32x32.png' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', href: 'images/favicon/favicon-16x16.png' },
+    { rel: 'manifest', href: 'images/favicon/site.webmanifest' },
+    { rel: 'mask-icon', href: 'images/favicon/safari-pinned-tab.svg', color: '#5bbad5' }
   ],
   htmlAttrs: {
     lang: 'en',
